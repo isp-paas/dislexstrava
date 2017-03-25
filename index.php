2d4e4c5589e2a55bb911ed084cf02379bbd7e28f
ISP-PaaS <alessandro.conconi@intesasanpaolo.com>
2017-03-26 00:56:09 +0100

    dev in corso...

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ try {
 
 
         print '<html><body>';
-        print '<head><link rel="stylesheet" href="/site.css">/head>';
+        print '<head><link rel="stylesheet" href="/site.css"></head>';
         print '<body>';
         print '<h1>Benvenuto in Dislex Strava</h1>';
         //print '<p>Token per autenticazione '.$token.'</p>';
