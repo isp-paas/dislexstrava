0a40b1896f79545f80ff0542ec6f277fdf013cbc
ISP-PaaS <alessandro.conconi@intesasanpaolo.com>
2017-03-26 00:23:06 +0100

    dev in corso...

--- presentation/printAthleteInfos.php
+++ presentation/printAthleteInfos.php
@@ -8,7 +8,7 @@ function printAthleteInfos($athlete){
 	$html =$html + "<li>City  : ".$athlete[city]."</li>";
 	$html =$html + "<li>State : ".$athlete[state]."</li>";
 	$html =$html + "</ul>";
-	$html =$html + "<img src=\"".$athlete[profile."\">";
+	$html =$html + "<img src=\"".$athlete[profile]."\">";
    //return $athlete;
     return $html;
 
