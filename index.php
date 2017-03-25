1fe290aee224f74dd1781ca4125b30ba54c382f5
ISP-PaaS <alessandro.conconi@intesasanpaolo.com>
2017-03-25 22:16:39 +0100

    dev in corso...

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ include 'vendor/autoload.php';
 use Strava\API\OAuth;
 use Strava\API\Exception;
 
+use Pest;
+use Strava\API\Client;
+use Strava\API\Service\REST;
+
+
 try {
     $options = array(
         'clientId'     => 14277,
@@ -22,9 +27,29 @@ try {
             'code' => $_GET['code']
         ));
 
+
         print '<html><body>';
         print '<h1>Benvenuto in Dislex Strava</h1>';
         print '<p>Token per autenticazione '.$token.'</p>';
+
+        try {
+            $adapter = new Pest('https://www.strava.com/api/v3');
+            $service = new REST($token, $adapter);  // Define your user token here..
+            $client = new Client($service);
+
+            $athlete = $client->getAthlete();
+            print_r($athlete);
+
+            $activities = $client->getAthleteActivities();
+            print_r($activities);
+
+            $club = $client->getClub(9729);
+            print_r($club);
+        } catch(Exception $e) {
+            print $e->getMessage();
+        }
+
+
         
         print '</body></html>';
     }
